5c344d054b2b76f06db8a985370469a378ee82f4
devipr53 <devipr53@gmail.com>
2020-06-26 18:08:48 +0530

    adding procfile

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export default{
     //MYSQL_URL:process.env.MYSQL_URL || 'jdbc:mysql://127.0.0.1:3306/react_db'
-    //PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 5000,
     MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost:27017/MyReactApp',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
 }
